dcac9227a8c56891453fcb01d92ecd7f6530f409
Gabriella Esteves <gabepk.ape@gmail.com>
2018-08-13 12:25:04 -0300

    Post detail

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ def post_list(request):
     #.filter(published_date__lte=timezone.now())
     posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
