d8bcaf6ff000b49646c188fbc2d54dfe698416cd
Gabriella Esteves <gabepk.ape@gmail.com>
2018-08-14 01:00:18 -0300

    Adicionando novos posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 """ Esse modulo define o(s) post(s) que serao mostrados nas views """
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 def post_list(request):
@@ -18,3 +19,21 @@ def post_detail(request, pk):
     """ Mostra post com primary key == pk """
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
+
+def post_new(request):
+    """ Form para adicao de novo post """
+    if request.method == "POST":
+        print('aqui 1')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            print('aqui 2')
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        print('aqui 3')
+        form = PostForm()
+    
+    return render(request, 'blog/post_edit.html', {'form':form})
