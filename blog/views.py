dfb5b5309cf8b9ae5a632ad24384b643de439361
Gabriella Esteves <gabepk.ape@gmail.com>
2018-08-14 00:14:01 -0300

    Mostrando erro 404 em posts nao encontrados

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+""" Esse modulo define o(s) post(s) que serao mostrados nas views """
+
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
-# Create your views here.
 def post_list(request):
-    #.filter(published_date__lte=timezone.now())
-    posts = Post.objects.order_by('published_date')
+    """ Mostra todos os posts com data de publicacao nao vazia """
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+#def post_detail(request, pk):
+#    """ Mostra post com primary key == pk """
+#    post = Post.objects.get(pk=pk)
+#    return render(request, 'blog/post_detail.html', {'post':post})
 
 def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
+    """ Mostra post com primary key == pk """
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
