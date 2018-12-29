40e2e7fe4d5d04ee31be41c30c0088c108d0c97c
vinicoreia <shadowtrolling@gmail.com>
2018-12-29 21:21:50 -0200

    Atualizando Roadmap

--- blog/static/js/roadmap.js
+++
@@ -1,9 +0,0 @@
-mermaid.initialize({
-    theme: 'forest',
-    // themeCSS: '.node rect { fill: red; }',
-    logLevel: 3,
-    flowchart: { curve: 'linear' },
-    gantt: { axisFormat: '%m/%d/%Y' },
-    sequence: { actorMargin: 50 },
-    // sequenceDiagram: { actorMargin: 300 } // deprecated
-});
