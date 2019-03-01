0fdd851326701caa3466bfac4e54abe887592d44
Gabriella Esteves <gabepk.ape@gmail.com>
2019-03-01 04:33:20 +0000

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,9 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'^ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('blog.urls')),
-]
-
-# DEVELOPMENT ONLY
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT) 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
