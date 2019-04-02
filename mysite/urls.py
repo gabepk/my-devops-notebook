2e45d27978ba2bf2d3649439401e1c4ef9a61731
vinicoreia <viniciusfilgf@gmail.com>
2019-04-02 17:38:22 -0300

    Urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
