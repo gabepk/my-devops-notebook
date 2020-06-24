d3ed9a796ecebd481d65fd917c558b204fe2b503
Gabriella Esteves <gabest@amazon.lu>
2020-06-24 23:21:07 +0200

    Add color attribute to Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Post
+from .models import Color
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Color)
--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.utils import timezone
 from ckeditor_uploader.fields import RichTextUploadingField
 
+class Color(models.Model):
+    color_name = models.CharField(max_length=20, default="Dark Red")
+    rgb = models.CharField(max_length=8, default="#cf0a2c")
+
+    def __str__(self):
+        return self.color_name
+
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = RichTextUploadingField()
-    slug = models.CharField(max_length=200, default="migrated-article")
+    slug = models.CharField(max_length=200, default="migrated-post")
+    color = models.ForeignKey("Color", default=0, on_delete=models.SET_DEFAULT)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
