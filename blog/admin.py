a75a339bbcc73fb2663ed247f1c07181e6f9b5fd
Gabriella Esteves <gabest@amazon.lu>
2020-06-27 04:53:30 +0200

    Verify changes in prod

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Post
 from .models import Color
+from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Color)
+admin.site.register(Post)
