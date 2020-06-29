db5554ef445d6773bb42e54e655fb1d1e6fef48a
Gabriella Esteves <gabest@amazon.lu>
2020-06-29 09:04:45 +0200

    Remove Color model

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Color
 from .models import KnowledgeLevel
 from .models import Post
 
 # Register your models here.
-admin.site.register(Color)
 admin.site.register(KnowledgeLevel)
 admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,6 @@ from django.db import models
 from django.utils import timezone
 from ckeditor_uploader.fields import RichTextUploadingField
 
-class Color(models.Model):
-    color_name = models.CharField(max_length=25, default="Dark Red")
-    rgb = models.CharField(max_length=8, default="#cf0a2c")
-
-    def __str__(self):
-        return self.color_name
-
 class KnowledgeLevel(models.Model):
     name = models.CharField(max_length=25, default="Very Low")
     color = models.CharField(max_length=8, default="#cf0a2c")
@@ -21,6 +14,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = RichTextUploadingField()
     slug = models.CharField(max_length=200, default="migrated-post")
+    knowledge_level = models.ForeignKey("KnowledgeLevel", default=1, on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
