83eed83d670f7839bc16a5e3df3b3c5fcbc4b8f9
Gabriella Esteves <gabest@amazon.lu>
2021-02-24 23:48:39 +0100

    Add subtitle in roadmap

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
